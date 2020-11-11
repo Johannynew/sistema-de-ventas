f333d045184f8730575fbed876b1fe7c88271dea
Mateo Acevedo <mateoacevedo.glowicom@gmail.com>
2020-11-10 23:40:43 -0400

    update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,22 +1,73 @@
 <template>
-  <q-page class="window-height window-width row justify-center items-center">
-    <div class="column">
-      <div>
-      </div>
+  <q-page
+    class="window-height window-width row justify-center items-center"
+    style="background: #fff;"
+  >
+    <div class="column q-pa-lg">
       <div class="row">
-        <q-card square bordered class="q-pa-lg shadow-1">
+        <q-card square class="shadow-24" style="width:300px;height:485px;">
+          <q-card-section class="bg-primary">
+            <h4 class="text-h5 text-white q-my-md">Repuestos Las Chicas</h4>
+            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+              <q-btn fab icon="add" color="blue" />
+            </div>
+          </q-card-section>
           <q-card-section>
-            <h5 class="text-h5" style="text-align:center; color:blue;">Repuestos Las Chicas</h5>
-            <q-form class="q-gutter-md">
-              <q-input square filled clearable v-model="email" type="email" label="email" />
-              <q-input square filled clearable v-model="password" type="password" label="password" />
+            <q-form class="q-px-sm q-pt-xl">
+              <q-input square clearable v-model="email" type="email" label="Correo">
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+              <q-input square clearable v-model="password" type="password" label="Clave">
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
             </q-form>
           </q-card-section>
-          <q-card-actions class="q-px-md">
-            <q-btn unelevated color="primary" size="lg" class="full-width" label="Login" />
+          <q-card-actions class="q-px-lg">
+            <q-btn unelevated size="lg" color="primary" class="full-width text-white" label="Ingresar" />
           </q-card-actions>
-          <q-card-section class="text-center q-pa-none">
-            <p class="text-grey-6">No Registrado? <a href="#" style="text-decoration:none; color:blue">Crear Cuenta</a></p>
+          <q-card-section class="text-center q-pa-sm">
+            <p class="text-grey-6">Olvido Su Contraseña?</p>
+          </q-card-section>
+        </q-card>
+      </div>
+    </div>
+    <div class="column q-pa-lg">
+      <div class="row">
+        <q-card square class="shadow-24" style="width:300px;height:485px;">
+          <q-card-section class="bg-primary">
+            <h4 class="text-h5 text-white q-my-md">Registro</h4>
+            <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+              <q-btn fab icon="close" color="blue" />
+            </div>
+          </q-card-section>
+          <q-card-section>
+            <q-form class="q-px-sm q-pt-xl q-pb-lg">
+              <q-input square clearable v-model="email" type="email" label="Correo">
+                <template v-slot:prepend>
+                  <q-icon name="email" />
+                </template>
+              </q-input>
+              <q-input square clearable v-model="username" type="username" label="Usuario">
+                <template v-slot:prepend>
+                  <q-icon name="person" />
+                </template>
+              </q-input>
+              <q-input square clearable v-model="password" type="password" label="Contraseña">
+                <template v-slot:prepend>
+                  <q-icon name="lock" />
+                </template>
+              </q-input>
+            </q-form>
+          </q-card-section>
+          <q-card-actions class="q-px-lg">
+            <q-btn unelevated size="lg" color="primary" class="full-width text-white" label="Registrar" />
+          </q-card-actions>
+          <q-card-section class="text-center q-pa-sm">
+            <p class="text-grey-6">Volver al Login</p>
           </q-card-section>
         </q-card>
       </div>
@@ -30,6 +81,7 @@ export default {
   data () {
     return {
       email: '',
+      username: '',
       password: ''
     }
   }
@@ -37,7 +89,5 @@ export default {
 </script>
 
 <style>
-.q-card {
-  width: 360px;
-}
 </style>
+@justinatack
